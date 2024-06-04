ae35ea53736c9e90f062576abe1fab9b5f23d5a9
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-04 22:09:21 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Layout from "./components/Layout/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import Pagenotfound from "./pages/Pagenotfound";
 
 function App() {
   return (
     <div>
-      <Layout>
-        <h2>hello</h2>
-      </Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "../../styles/HeaderStyle.css";
+import { AppBar, Box, Typography, Toolbar } from "@mui/material";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-    <h1> header</h1>
-    </div>
-  )
-}
+    <Box>
+      <AppBar component="nav" sx={{ backgroundColor: "black" }}>
+        <Toolbar>
+          <FastfoodIcon sx={{ color: "goldenrod", marginRight: 1 }} />
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            My Restaurant
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <ul className="navigation-menu">
+              <li>
+                <Link to={"/"}> Home</Link>
+              </li>
+              <li>
+                <Link to={"/about"}> About Us</Link>
+              </li>
+              <li>
+                <Link to={"/menu"}>Menu</Link>
+              </li>
+              <li>
+                <Link to={"/contact"}>Contact</Link>
+              </li>
+            </ul>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
